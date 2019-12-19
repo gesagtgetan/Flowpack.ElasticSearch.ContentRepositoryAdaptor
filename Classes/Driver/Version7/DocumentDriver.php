222d0c068e3e5ead2c9c9d58c0b92ef2e4faebfb
Florian Lammel <florian.lammel@gesagt-getan.at>
2019-12-19 16:10:19 +0100

    FEATURE: Add driver for Elasticsearch 7.x

---
+++ Classes/Driver/Version7/DocumentDriver.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version7;
+
+/*
+ * This file is part of the Flowpack.ElasticSearch.ContentRepositoryAdaptor package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\AbstractDriver;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\DocumentDriverInterface;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\NodeTypeMappingBuilderInterface;
+use Flowpack\ElasticSearch\Domain\Model\Index;
+use Flowpack\ElasticSearch\Domain\Model\Mapping;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Model\NodeType;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Document driver for Elasticsearch version 5.x
+ *
+ * @Flow\Scope("singleton")
+ */
+class DocumentDriver extends AbstractDriver implements DocumentDriverInterface
+{
+    /**
+     * @Flow\Inject
+     * @var NodeTypeMappingBuilderInterface
+     */
+    protected $nodeTypeMappingBuilder;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(NodeInterface $node, string $identifier): array
+    {
+        return [
+            [
+                'delete' => [
+                    '_id' => $identifier
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws \Flowpack\ElasticSearch\Exception
+     */
+    public function deleteDuplicateDocumentNotMatchingType(Index $index, string $documentIdentifier, NodeType $nodeType): void
+    {
+        $result = $index->request('GET', '/_search?scroll=1m', [], json_encode([
+            'sort' => ['_doc'],
+            'query' => [
+                'bool' => [
+                    'must' => [
+                        'ids' => [
+                            'values' => [$documentIdentifier]
+                        ]
+                    ],
+                    'must_not' => [
+                        'term' => [
+                            Mapping::TYPE_PROPERTY_NAME => $this->nodeTypeMappingBuilder->convertNodeTypeNameToMappingName($nodeType->getName())
+                        ]
+                    ]
+                ]
+            ]
+        ]));
+        $treatedContent = $result->getTreatedContent();
+        $scrollId = $treatedContent['_scroll_id'];
+        $mapHitToDeleteRequest = function ($hit) {
+            return json_encode([
+                'delete' => [
+                    '_id' => $hit['_id']
+                ]
+            ]);
+        };
+        $bulkRequest = [];
+        while (isset($treatedContent['hits']['hits']) && $treatedContent['hits']['hits'] !== []) {
+            $hits = $treatedContent['hits']['hits'];
+            $bulkRequest = array_merge($bulkRequest, array_map($mapHitToDeleteRequest, $hits));
+            $result = $index->request('GET', '/_search/scroll?scroll=1m', [], ['scroll_id' => $scrollId], false);
+            $treatedContent = $result->getTreatedContent();
+        }
+        $this->logger->log(sprintf('NodeIndexer: Check duplicate nodes for %s (%s), found %d document(s)', $documentIdentifier, $nodeType->getName(), count($bulkRequest)), LOG_DEBUG, null, 'ElasticSearch (CR)');
+        if ($bulkRequest !== []) {
+            $index->request('POST', '/_bulk', [], implode("\n", $bulkRequest) . "\n");
+        }
+        $this->searchClient->request('DELETE', '/_search/scroll', [], json_encode([
+            'scroll_id' => [
+                $scrollId
+            ]
+        ]));
+    }
+}
